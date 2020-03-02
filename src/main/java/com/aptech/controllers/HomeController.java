dd031b380b3166ebde5d64c00027f6360ddc9ed5
Krishna <krishnayamphu@gmail.,com>
2020-03-02 23:26:56 +0545

    homepage, css

--- src/main/java/com/aptech/controllers/HomeController.java
+++ src/main/java/com/aptech/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package com.aptech.controllers;
 
+import com.aptech.dao.PostDao;
+import com.aptech.models.Post;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/home")
 public class HomeController extends HttpServlet {
@@ -14,6 +18,8 @@ public class HomeController extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        List<Post> allPosts = PostDao.getAllPosts();
+        request.setAttribute("allPosts", allPosts);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AptechOnline | Home</title>
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <nav>
+            <a class="brand" href="">AptechOnline</a>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Politics</a></li>
+                <li><a href="#">LifeStyle</a></li>
+                <li><a href="#">Entertainment</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+</body>
+</html>
